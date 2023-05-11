fa6ab7b630892b3217d7f70981a39f86d09088f1
sumancfc <sumanstha999@gmail.com>
2023-05-11 23:00:50 +0200

    update

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
-
 const port = process.env.PORT || 8000;
-
 const uri = process.env.URL;
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
-//connect to mongodb
+//connect to database
 async function connection() {
   try {
     await client.connect();
-    console.log("You successfully connected to MongoDB!");
+    console.log("Connected to mongodb database");
   } catch (err) {
     console.log(err);
   }
@@ -21,16 +19,21 @@ async function connection() {
 
 app.use(express.json());
 
+const movies = client.db("movie").collection("movies");
+
 app.get("/", (req, res) => {
   res.send({
-    GET: "/movies",POST: "/movie/add",GET: "/movie/:id",PUT: "/movie/:id",DELETE: "/movie/:id"});
+    GET: "/movies",
+    POST: "/movie/add",
+    GET: "/movie/:id",
+    PUT: "/movie/:id",
+    DELETE: "/movie/:id",
+  });
 });
 
 // get all the movies
 // method: GET
 app.get("/movies", async (req, res) => {
-  const movies = client.db("movie").collection("movies");
-
   const allMoviesList = await movies.find().toArray();
 
   res.status(200).json(allMoviesList);
@@ -39,7 +42,6 @@ app.get("/movies", async (req, res) => {
 // add movie in the list
 // method: POST
 app.post("/movie/add", async (req, res) => {
-  const movies = client.db("movie").collection("movies");
   const { name, director, actor, actress, genre, year } = req.body;
 
   const movie = await movies.insertOne({
@@ -57,7 +59,6 @@ app.post("/movie/add", async (req, res) => {
 //get single movie by id
 // method: GET
 app.get("/movie/:id", async (req, res) => {
-  const movies = client.db("movie").collection("movies");
   const { id } = req.params;
 
   const movie = await movies.findOne({ _id: new ObjectId(id) });
@@ -67,7 +68,6 @@ app.get("/movie/:id", async (req, res) => {
 // update movie using id
 // method: PUT
 app.put("/movie/:id", async (req, res) => {
-  const movies = client.db("movie").collection("movies");
   const { id } = req.params;
   const updateMovie = req.body;
 
@@ -82,13 +82,13 @@ app.put("/movie/:id", async (req, res) => {
 // delete movie using id
 // method: DELETE
 app.delete("/movie/:id", async (req, res) => {
-  const movies = client.db("movie").collection("movies");
   const { id } = req.params;
 
   await movies.deleteOne({ _id: new ObjectId(id) });
   res.status(200).json("Movie has been deleted.");
 });
 
+// run the server
 connection()
   .then(() => {
     app.listen(port, () => {
